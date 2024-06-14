01a49e2b5f9f047daf52ff66494d60af38471513
LeeDonggeun <121033188+Leecarrot@users.noreply.github.com>
2024-06-14 23:00:26 +0900

    Update train.py
    recall 및 f1 score 추가

--- train.py
+++ train.py
@@ -5,22 +5,19 @@ from preprocessing import load_data
 import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
-from sklearn.metrics import f1_score
-
-print(torch.cuda.is_available())
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+from sklearn.metrics import recall_score, f1_score
 
 def calculate_accuracy(y_pred, y_true):
     predicted = torch.argmax(y_pred, dim=1)
-    correct = (predicted == y_true).float()
-    accuracy = correct.sum() / len(correct)
+    correct = (predicted == y_true).sum().item()
+    accuracy = correct / y_true.size(0)
     return accuracy
 
 def save_model(model, path):
     torch.save(model.state_dict(), path)
 
 def main():
-    dataset = load_data('data2.csv')
+    dataset = load_data('구매전환율_data.csv')
     train_size = int(0.8 * len(dataset))
     test_size = len(dataset) - train_size
     train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
@@ -28,61 +25,67 @@ def main():
     train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-    ## 데이터에 맞게 dimension 수정 필요
-    ## 현재 기존 데이터셋에 맞게 설정
-    model = init_model(input_dim=4, hidden_dim=64, output_dim=3, num_layers=2, num_heads=4, dropout=0.3)
-    model = model.to(device)
+    model = init_model(input_dim=9, hidden_dim=64, output_dim=2, num_layers=2, num_heads=4 , dropout=0.3)
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     criterion = nn.CrossEntropyLoss()
 
-    for epoch in range(100):
+    for epoch in range(15):
         model.train()
         train_loss = 0
-        train_acc = 0
-        train_preds = []
-        train_labels = []
+        correct = 0
+        total = 0
+        all_train_labels = []
+        all_train_preds = []
+        
         for features, labels in train_loader:
-            features, labels = features.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = model(features.float())
             loss = criterion(outputs, labels)
-            acc = calculate_accuracy(outputs, labels)
-            preds = torch.argmax(outputs, dim=1)
-            train_preds.extend(preds.cpu().numpy())
-            train_labels.extend(labels.cpu().numpy())
             loss.backward()
             optimizer.step()
+            
             train_loss += loss.item()
-            train_acc += acc.item()
+            preds = torch.argmax(outputs, dim=1)
+            correct += (preds == labels).sum().item()
+            total += labels.size(0)
+            
+            all_train_labels.extend(labels.cpu().numpy())
+            all_train_preds.extend(preds.cpu().numpy())
+
+        train_acc = correct / total
+        train_recall = recall_score(all_train_labels, all_train_preds, average='macro', zero_division=0)
+        train_f1 = f1_score(all_train_labels, all_train_preds, average='macro', zero_division=0)
         train_loss /= len(train_loader)
-        train_acc /= len(train_loader)
-        train_f1 = f1_score(train_labels, train_preds, average='macro')
 
         model.eval()
         with torch.no_grad():
             test_loss = 0
-            test_acc = 0
-            all_preds = []
-            all_labels = []
+            correct = 0
+            total = 0
+            all_test_labels = []
+            all_test_preds = []
+            
             for features, labels in test_loader:
-                features, labels = features.to(device), labels.to(device)
                 outputs = model(features.float())
                 loss = criterion(outputs, labels)
-                acc = calculate_accuracy(outputs, labels)
+                
                 test_loss += loss.item()
-                test_acc += acc.item()
                 preds = torch.argmax(outputs, dim=1)
-                all_preds.extend(preds.cpu().numpy())
-                all_labels.extend(labels.cpu().numpy())
+                correct += (preds == labels).sum().item()
+                total += labels.size(0)
+                
+                all_test_labels.extend(labels.cpu().numpy())
+                all_test_preds.extend(preds.cpu().numpy())
+
+            test_acc = correct / total
+            test_recall = recall_score(all_test_labels, all_test_preds, average='macro', zero_division=0)
+            test_f1 = f1_score(all_test_labels, all_test_preds, average='macro', zero_division=0)
             test_loss /= len(test_loader)
-            test_acc /= len(test_loader)
-            test_f1 = f1_score(all_labels, all_preds, average='macro')
 
-        print(f'Epoch {epoch+1}: Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f}, Train F1 Score: {train_f1:.4f}, Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}, Test F1 Score: {test_f1:.4f}')
+        print(f'Epoch {epoch+1}: Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f}, Train Recall: {train_recall:.4f}, Train F1: {train_f1:.4f}')
+        print(f'Epoch {epoch+1}: Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}, Test Recall: {test_recall:.4f}, Test F1: {test_f1:.4f}')
 
-    save_model(model, './trained_model.pth')
+    save_model(model, './trained_model_purchase(구매전환율).pth')
 
 if __name__ == "__main__":
     main()
-
-
